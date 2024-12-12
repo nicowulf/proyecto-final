dd3d3db9d1d3904fd5da20983daf2e1172c793d0
[nicowulf] <[n.wulfsztat@gmail.com]>
2024-12-11 23:17:54 -0300

    add: users authentication

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,61 @@
+import AuthModel from "../models/authModel.js";
+import jwt from "jsonwebtoken";
+
+process.loadEnvFile();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const register = async (req, res) => {
+  try {
+    const { username, password, email } = req.body;
+    if (!username || !password || !email) {
+      return res.status(400).json({ error: "Bad request, missing data" });
+    }
+
+    const newUser = await authModel.register({ username, password, email });
+    if (newUser === null) {
+      return res.status(400).json({ error: "User already exists, take another" });
+    }
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Both fields required" });
+    }
+
+    const user = await authModel.login({ username, password });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      JWT_SECRET,
+      { expiresIn: "1h" }
+    ); 
+
+    return res.status(200).json({
+      message: "Login successfully",
+      user: { id: user._id, username: user.username },
+      token,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
+  }
+};
+
+export { register, login };
--- backend/src/routes/articleRouter.js
+++ backend/src/routes/articleRouter.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { createArticle, getAllArticles } from "../controllers/articleController.js"
+import {
+  createArticle,
+  getAllArticles, 
+  getArticleById,
+  updateArticle,
+  deleteArticle,
+} from "../controllers/articleController.js";
+import { auth } from "../middleware/authMiddleware.js";
 
 const articleRouter = Router()
 
 articleRouter.get("/", getAllArticles)
+articleRouter.get("/:id", getArticleById);
 articleRouter.post("/", createArticle)
+articleRouter.put("/:id", updateArticle);
+articleRouter.delete("/:id", deleteArticle);
 
 
 export { articleRouter }
